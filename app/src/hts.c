751fb806d1125d29ca51c2a82769bc70d4ea8a0b
Mateusz Sierszulski <mateusz.sierszulski@gmail.com>
2023-01-12 18:49:26 +0100

    Update app
    Signed-off-by: Mateusz Sierszulski <mateusz.sierszulski@gmail.com>

--- app/src/hts.c
+++ app/src/hts.c
@@ -59,8 +59,8 @@ void indicate(void)
 	struct sensor_value temp_value;
 
 	static uint8_t htm[5];
-	static uint16_t temperature = 0U;
-	static uint16_t humid = 0U;
+	static double temperature = 0U;
+	static double humid = 0U;
 	uint32_t mantissa;
 	uint8_t exponent;
 	int r;
@@ -85,15 +85,21 @@ void indicate(void)
 	ind_params.attr = &hts_svc.attrs[2];
 	ind_params.func = indicate_cb;
 	ind_params.destroy = indicate_destroy;
-	ind_params.data = &temperature;
-	ind_params.len = sizeof(temperature);
+	ind_params.data = &htm;
+	ind_params.len = sizeof(htm);
 	if (bt_gatt_indicate(NULL, &ind_params) == 0) {
 		indicating = 1U;
 	}
+	mantissa = (uint32_t)(humid * 100);
+	exponent = (uint8_t)-2;
+
+	htm[0] = 0; /* temperature in celsius */
+	sys_put_le24(mantissa, (uint8_t *)&htm[1]);
+	htm[4] = exponent;
 
 	ind_params.attr = &hts_svc.attrs[4];
-	ind_params.data = &humid;
-	ind_params.len = sizeof(humid);
+	ind_params.data = &htm;
+	ind_params.len = sizeof(htm);
 
 	if (bt_gatt_indicate(NULL, &ind_params) == 0) {
 		indicating = 1U;
